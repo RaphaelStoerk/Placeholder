d8bc570cd85cac4e268467b49704b2f389d594ca
Raphael <raphael.stoerk@uni-ulm.de>
2019-11-06 23:29:53 +0100

    added the algorithm example and the notes shown in the last tut

---
+++ res/Example.java
@@ -0,0 +1,47 @@
+public class Example {
+
+  // Start Algorithmus
+  public static void main(String[] args) {
+
+    // gegeben: p, c, s als positive Ganzzahlen
+    int points = 10;
+    int coins = 15;
+    int stars = 8;
+
+    // addiere p / 10 zu c hinzu
+    coins = coins + points / 10;
+
+    // solange c > 1
+    while(coins > 1) {
+
+      // falls c gerade
+      if(coins % 2 == 0) {
+
+        // addiere 1 zu s hinzu
+        stars = stars + 1;
+        // halbiere c
+        coins = coins / 2;
+
+      } else {
+        // beende wiederholung
+        break;
+      } // end if
+
+    } // end loop
+
+    // falls s > 10
+    if(stars > 10) {
+
+      // Spieler hat gewonnen
+      System.out.println("the player has won");
+
+    // Ansonsten
+    } else {
+
+      // Spieler hat verloren
+      System.out.println("the player has lost");
+
+    }
+  }
+  // Ende Algorithmus
+}
