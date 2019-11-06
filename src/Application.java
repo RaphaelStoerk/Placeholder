3accc2349c334b15343703fcdfb3cbb84502fe95
Raphael <raphael.stoerk@uni-ulm.de>
2019-11-06 23:19:35 +0100

    initial commit
    added all previously generated files to the repository

---
+++ src/Application.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Application {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    String name = scan.nextLine();
+    System.out.println("Hallo " + name + " viel Spaß beim spielen");
+  }
+}
