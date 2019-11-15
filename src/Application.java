4365290517985f4504d461cb8786dde7d619a6e2
Raphael <raphael.stoerk@uni-ulm.de>
2019-11-15 09:51:42 +0100

    Added a simple menu to our application

--- src/Application.java
+++ src/Application.java
@@ -14,24 +14,35 @@ public class Application {
   * @param args input values from the command line
   */
   public static void main(String[] args) {
+
     // create a new Scanner that lets us read in input from the user
     Scanner scan = new Scanner(System.in);
-
-    // ask the user to input their name
     System.out.print("Welcome to <placeholder>\nPlease enter your name\n\t::> ");
-
-    // read in the name of the user
     String name = scan.nextLine();
-
-    // greet the user
-    // TODO: Fix input bug
     System.out.println("Hello " + name + ", have fun playing this game!");
 
-    // TODO: Create a menu for the user to chose from
+    boolean running = true;
 
-    
+    // The menu loop
+    while(running) {
+      System.out.println("You have the following options:");
+      System.out.println("(1) Start Game");
+      System.out.println("(2) Options");
+      System.out.println("(3) End Application");
 
-    // say goodbye to the user
+      int opt = scan.nextInt();
+      System.out.println("You have entered option: " + opt);
+
+      // TODO: consider using a switch statement
+      if(opt == 1) {
+        System.out.println("This is a game");
+      } else if(opt == 2) {
+        System.out.println("This is options");
+      } else if(opt == 3) {
+        running = false;
+      }
+    }
+
     System.out.println("Goodbye " + name + ", it seems that this is all for now!");
   }
 }
